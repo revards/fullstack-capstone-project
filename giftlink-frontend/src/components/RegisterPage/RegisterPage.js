433288934f5790b434d4dc3eae33086d2c0e069a
revards <revards@users.noreply.github.com>
2025-06-09 06:39:00 -0400

    Register and state

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
-
+//Step 1 - Task 1
+import { urlConfig } from '../../config';
+//Step 1 - Task 2
+import { useAppContext } from '../../context/AuthContext';
+//Step 1 - Task 3
+import { useNavigate } from 'react-router-dom';
 import './RegisterPage.css';
 
 function RegisterPage() {
@@ -8,10 +13,47 @@ function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleRegister = async () => {
-        console.log("Register invoked")
-    }
+    //Step 1 - Task 4
+    const [showerr, setShowerr] = useState('');
+    //Step 1 - Task 5
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
 
+    const handleRegister = async () => {
+        const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+            //Step 1 - Task 6
+            method: 'POST',
+            //Step 1 - Task 7
+            headers: {
+                'content-type': 'application/json',
+            },
+            //Step 1 - Task 8
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            })
+        })
+        //Step 2 - Task 1
+        const json = await response.json();
+        console.log('json data', json);
+        console.log('er', json.error);
+        //Step 2 - Task 2
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', firstName);
+            sessionStorage.setItem('email', json.email);
+            //Step 2 - Task 3
+            setIsLoggedIn(true);
+            //Step 2 - Task 4
+            navigate('/app');
+        }
+        if (json.error) {
+            //Step 2 - Task 5
+            setShowerr(json.error);
+        }
+    }
     return (
         <div className="container mt-5">
             <div className="row justify-content-center">
@@ -29,9 +71,7 @@ function RegisterPage() {
                                 onChange={(e) => setFirstName(e.target.value)}
                             />
                         </div>
-
                         {/* last name */}
-
                         <div className="mb-3">
                             <label htmlFor="lastName" className="form-label">LastName</label>
                             <input
@@ -43,7 +83,6 @@ function RegisterPage() {
                                 onChange={(e) => setLastName(e.target.value)}
                             />
                         </div>
-
                         {/* email  */}
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email</label>
@@ -55,8 +94,9 @@ function RegisterPage() {
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
+                            {/* Step 2 - Task 6*/}
+                            <div className="text-danger">{showerr}</div>
                         </div>
-
                         <div className="mb-4">
                             <label htmlFor="password" className="form-label">Password</label>
                             <input
