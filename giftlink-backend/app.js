63cc557ba7b25de06d3e012b6a169b4ed5bcf2c2
revards <revards@users.noreply.github.com>
2025-06-09 05:01:55 -0400

    Auth routes for register made

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const authRoutes = require('./routes/authRoutes');
 
 const connectToDatabase = require('./models/db');
 const { loadData } = require("./util/import-mongo/index");
@@ -25,8 +26,7 @@ app.use(express.json());
 const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
+app.use('/api/search', searchRoutes);
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -38,7 +38,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
